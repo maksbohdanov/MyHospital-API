c70301f2601dcca0d5c3b13c6a99342a30053f41
Maksym Bohdanov <maks.bohdanov27@gmail.com>
2022-12-16 12:39:29 +0200

    Seed data

--- DAL/HospitalDbContext.cs
+++ DAL/HospitalDbContext.cs
@@ -54,6 +54,75 @@ namespace DAL
             modelBuilder.Entity<Patient>()
                 .HasIndex(p => p.Phone)
                 .IsUnique(true);
+
+            SeedData(modelBuilder);
         }
-    }
+
+        private void SeedData(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<FavorName>().HasData(
+                new FavorName() { Id = 1, Name = "Первинна консультація" },
+                new FavorName() { Id = 2, Name = "Повторна консультація" },
+                new FavorName() { Id = 3, Name = "Лабораторні дослідження" },
+                new FavorName() { Id = 4, Name = "Ультразвукова діагностика" },
+                new FavorName() { Id = 5, Name = "Рентгенографія" },
+                new FavorName() { Id = 6, Name = "Комп'ютерна томографія" });
+
+            modelBuilder.Entity<FavorType>().HasData(
+                new FavorType() { Id = 1, Type = "Консультація" },
+                new FavorType() { Id = 2, Type = "Діагностика" });
+
+            modelBuilder.Entity<Specialization>().HasData(
+                new Specialization() { Id = 1, Title = "Дослідження та діагностика" },
+                new Specialization() { Id = 2, Title = "Терапія" },
+                new Specialization() { Id = 3, Title = "Хірургія" },
+                new Specialization() { Id = 4, Title = "Педіатрія" },
+                new Specialization() { Id = 5, Title = "Дерматологія" },
+                new Specialization() { Id = 6, Title = "Психотерапія" });
+
+            modelBuilder.Entity<Patient>().HasData(
+                new Patient() { Id = 1, FullName = "Петренко Петро", Phone = "380961234567" },
+                new Patient() { Id = 2, FullName = "Іваненко Іван", Phone = "380501234567" },
+                new Patient() { Id = 3, FullName = "Семененко Семен", Phone = "380661234567" },
+                new Patient() { Id = 4, FullName = "Ольченко Ольга", Phone = "380931234567" });
+
+            modelBuilder.Entity<Doctor>().HasData(
+                new Doctor() { Id = 1, FirstName = "Анна", LastName = "Мельник", MiddleName = "Олексіївна", SpecializationId = 1, Experience = 3 },
+                new Doctor() { Id = 2, FirstName = "Юрій", LastName = "Дрогобич", MiddleName = "Михайлович", SpecializationId = 2, Experience = 10 },
+                new Doctor() { Id = 3, FirstName = "Микола", LastName = "Амосов", MiddleName = "Михайлович", SpecializationId = 3, Experience = 15 },
+                new Doctor() { Id = 4, FirstName = "Олександр", LastName = "Тур", MiddleName = "Федорович", SpecializationId = 4, Experience = 12 },
+                new Doctor() { Id = 5, FirstName = "Сергій", LastName = "Шевченко", MiddleName = "Олександрович", SpecializationId = 5, Experience = 7 },
+                new Doctor() { Id = 6, FirstName = "Ольга", LastName = "Кравчук", MiddleName = "Ігорівна", SpecializationId = 6, Experience = 9 });
+
+            modelBuilder.Entity<Favor>().HasData(
+                new Favor() { Id = 1, FavorTypeId = 2, FavorNameId = 3, SpecializationId = 1, Price = 200 },
+                new Favor() { Id = 2, FavorTypeId = 2, FavorNameId = 4, SpecializationId = 1, Price = 500 },
+                new Favor() { Id = 3, FavorTypeId = 2, FavorNameId = 5, SpecializationId = 1, Price = 400 },
+                new Favor() { Id = 4, FavorTypeId = 2, FavorNameId = 6, SpecializationId = 1, Price = 700 },
+
+                new Favor() { Id = 5, FavorTypeId = 1, FavorNameId = 1, SpecializationId = 2, Price = 350 },
+                new Favor() { Id = 6, FavorTypeId = 1, FavorNameId = 2, SpecializationId = 2, Price = 300 },
+
+                new Favor() { Id = 7, FavorTypeId = 1, FavorNameId = 1, SpecializationId = 3, Price = 350 },
+                new Favor() { Id = 8, FavorTypeId = 1, FavorNameId = 2, SpecializationId = 3, Price = 300 },
+
+                new Favor() { Id = 9, FavorTypeId = 1, FavorNameId = 1, SpecializationId = 4, Price = 300 },
+                new Favor() { Id = 10, FavorTypeId = 1, FavorNameId = 2, SpecializationId = 4, Price = 250 },
+
+                new Favor() { Id = 11, FavorTypeId = 1, FavorNameId = 1, SpecializationId = 5, Price = 350 },
+                new Favor() { Id = 12, FavorTypeId = 1, FavorNameId = 2, SpecializationId = 5, Price = 300 },
+
+                new Favor() { Id = 13, FavorTypeId = 1, FavorNameId = 1, SpecializationId = 6, Price = 400 },
+                new Favor() { Id = 14, FavorTypeId = 1, FavorNameId = 2, SpecializationId = 6, Price = 350 });
+
+            modelBuilder.Entity<Appointment>().HasData(
+                new Appointment() { Id = 1, DoctorId = 1, FavorId = 1, PatientId = 1, Date = new DateTime(2022, 12, 12, 15, 0, 0) },
+                new Appointment() { Id = 2, DoctorId = 1, FavorId = 2, PatientId = 1, Date = new DateTime(2022, 12, 12, 16, 0, 0) },
+                new Appointment() { Id = 3, DoctorId = 1, FavorId = 3, PatientId = 2, Date = new DateTime(2022, 12, 12, 17, 0, 0) },
+                new Appointment() { Id = 4, DoctorId = 2, FavorId = 5, PatientId = 1, Date = new DateTime(2022, 12, 12, 17, 0, 0) },
+                new Appointment() { Id = 5, DoctorId = 3, FavorId = 7, PatientId = 2, Date = new DateTime(2022, 12, 12, 17, 0, 0) },
+                new Appointment() { Id = 6, DoctorId = 4, FavorId = 9, PatientId = 3, Date = new DateTime(2022, 12, 12, 17, 0, 0) },
+                new Appointment() { Id = 7, DoctorId = 5, FavorId = 11, PatientId = 4, Date = new DateTime(2022, 12, 12, 17, 0, 0) });
+        }
+    }   
 }
---
+++ DAL/Migrations/20221216103818_SeedData.cs
@@ -0,0 +1,350 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Prive",
+                table: "Favors",
+                newName: "Price");
+
+            migrationBuilder.InsertData(
+                table: "FavorNames",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Первинна консультація" },
+                    { 2, "Повторна консультація" },
+                    { 3, "Лабораторні дослідження" },
+                    { 4, "Ультразвукова діагностика" },
+                    { 5, "Рентгенографія" },
+                    { 6, "Комп'ютерна томографія" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "FavorTypes",
+                columns: new[] { "Id", "Type" },
+                values: new object[,]
+                {
+                    { 1, "Консультація" },
+                    { 2, "Діагностика" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Patients",
+                columns: new[] { "Id", "FullName", "Phone" },
+                values: new object[,]
+                {
+                    { 1, "Петренко Петро", "380961234567" },
+                    { 2, "Іваненко Іван", "380501234567" },
+                    { 3, "Семененко Семен", "380661234567" },
+                    { 4, "Ольченко Ольга", "380931234567" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Specializations",
+                columns: new[] { "Id", "Title" },
+                values: new object[,]
+                {
+                    { 1, "Дослідження та діагностика" },
+                    { 2, "Терапія" },
+                    { 3, "Хірургія" },
+                    { 4, "Педіатрія" },
+                    { 5, "Дерматологія" },
+                    { 6, "Психотерапія" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Doctors",
+                columns: new[] { "Id", "Experience", "FirstName", "LastName", "MiddleName", "SpecializationId" },
+                values: new object[,]
+                {
+                    { 1, 3, "Анна", "Мельник", "Олексіївна", 1 },
+                    { 2, 10, "Юрій", "Дрогобич", "Михайлович", 2 },
+                    { 3, 15, "Микола", "Амосов", "Михайлович", 3 },
+                    { 4, 12, "Олександр", "Тур", "Федорович", 4 },
+                    { 5, 7, "Сергій", "Шевченко", "Олександрович", 5 },
+                    { 6, 9, "Ольга", "Кравчук", "Ігорівна", 6 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Favors",
+                columns: new[] { "Id", "FavorNameId", "FavorTypeId", "Price", "SpecializationId" },
+                values: new object[,]
+                {
+                    { 1, 3, 2, 200m, 1 },
+                    { 2, 4, 2, 500m, 1 },
+                    { 3, 5, 2, 400m, 1 },
+                    { 4, 6, 2, 700m, 1 },
+                    { 5, 1, 1, 350m, 2 },
+                    { 6, 2, 1, 300m, 2 },
+                    { 7, 1, 1, 350m, 3 },
+                    { 8, 2, 1, 300m, 3 },
+                    { 9, 1, 1, 300m, 4 },
+                    { 10, 2, 1, 250m, 4 },
+                    { 11, 1, 1, 350m, 5 },
+                    { 12, 2, 1, 300m, 5 },
+                    { 13, 1, 1, 400m, 6 },
+                    { 14, 2, 1, 350m, 6 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Appointments",
+                columns: new[] { "Id", "Date", "DoctorId", "FavorId", "PatientId" },
+                values: new object[,]
+                {
+                    { 1, new DateTime(2022, 12, 12, 15, 0, 0, 0, DateTimeKind.Unspecified), 1, 1, 1 },
+                    { 2, new DateTime(2022, 12, 12, 16, 0, 0, 0, DateTimeKind.Unspecified), 1, 2, 1 },
+                    { 3, new DateTime(2022, 12, 12, 17, 0, 0, 0, DateTimeKind.Unspecified), 1, 3, 2 },
+                    { 4, new DateTime(2022, 12, 12, 17, 0, 0, 0, DateTimeKind.Unspecified), 2, 5, 1 },
+                    { 5, new DateTime(2022, 12, 12, 17, 0, 0, 0, DateTimeKind.Unspecified), 3, 7, 2 },
+                    { 6, new DateTime(2022, 12, 12, 17, 0, 0, 0, DateTimeKind.Unspecified), 4, 9, 3 },
+                    { 7, new DateTime(2022, 12, 12, 17, 0, 0, 0, DateTimeKind.Unspecified), 5, 11, 4 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Appointments",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Appointments",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Appointments",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Appointments",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Appointments",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Appointments",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Appointments",
+                keyColumn: "Id",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Doctors",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Favors",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Favors",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Favors",
+                keyColumn: "Id",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "Favors",
+                keyColumn: "Id",
+                keyValue: 10);
+
+            migrationBuilder.DeleteData(
+                table: "Favors",
+                keyColumn: "Id",
+                keyValue: 12);
+
+            migrationBuilder.DeleteData(
+                table: "Favors",
+                keyColumn: "Id",
+                keyValue: 13);
+
+            migrationBuilder.DeleteData(
+                table: "Favors",
+                keyColumn: "Id",
+                keyValue: 14);
+
+            migrationBuilder.DeleteData(
+                table: "Doctors",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Doctors",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Doctors",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Doctors",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Doctors",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "FavorNames",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "FavorNames",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Favors",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Favors",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Favors",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Favors",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Favors",
+                keyColumn: "Id",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Favors",
+                keyColumn: "Id",
+                keyValue: 9);
+
+            migrationBuilder.DeleteData(
+                table: "Favors",
+                keyColumn: "Id",
+                keyValue: 11);
+
+            migrationBuilder.DeleteData(
+                table: "Patients",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Patients",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Patients",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Patients",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Specializations",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "FavorNames",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "FavorNames",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "FavorNames",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "FavorNames",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "FavorTypes",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "FavorTypes",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Specializations",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Specializations",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Specializations",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Specializations",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Specializations",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.RenameColumn(
+                name: "Price",
+                table: "Favors",
+                newName: "Prive");
+        }
+    }
+}
