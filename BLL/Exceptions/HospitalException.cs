ce3ec3987773d74eea9c637ec4144a8c3e2b2f56
Maksym Bohdanov <maks.bohdanov27@gmail.com>
2022-12-19 14:38:14 +0200

    Define custom exception

---
+++ BLL/Exceptions/HospitalException.cs
@@ -0,0 +1,17 @@
+﻿namespace BLL.Exceptions
+{
+    public class HospitalException: Exception
+    {
+        public HospitalException()
+        {
+        }
+
+        public HospitalException(string message) : base(message)
+        {
+        }
+
+        public HospitalException(string message, Exception inner) : base(message, inner)
+        {
+        }
+    }
+}
